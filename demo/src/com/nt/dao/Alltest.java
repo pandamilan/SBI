8a923e802ba58b45f2d7032b2c9bd9a495a430ec
milan <milan@ADMINRG-FMAUVKG>
2018-04-26 17:43:56 +0530

    nice short

---
+++ demo/src/com/nt/dao/Alltest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.nt.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author milan
+ *
+ */
+public class Alltest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ demo/src/com/nt/dao/test.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.nt.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author milan
+ *
+ */
+public class test {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
